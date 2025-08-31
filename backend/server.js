c16db557068f16ddaec757080904615cb890aab5
Uglycrown <gourav.singh9756@gmail.com>
2025-08-31 21:48:41 +0530

    fix: simplify CORS configuration by removing allowed origins

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,7 @@ const rangeParser = require('range-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ['https://detective-gen-z-music.vercel.app', 'http://localhost:5173'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
 app.use(express.json());
 
 const musicDir = process.env.MUSIC_DIR || path.join(__dirname, 'music');
